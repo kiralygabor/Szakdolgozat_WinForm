d421395b34c4cb55afeb167543ef17b41ecdf6a1
3111TAN-03 <3111TAN-03@tanuzem.boronkay.local>
2026-01-29 10:03:00 +0100

    Create Email sending app

--- MiniJobzAdminSite/Email.Designer.cs
+++ MiniJobzAdminSite/Email.Designer.cs
@@ -28,20 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SubjectLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.fileBtn = new System.Windows.Forms.Button();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleBox = new System.Windows.Forms.ComboBox();
+            this.attachmentLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(12, 72);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(39, 18);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Cím:";
+            // 
+            // SubjectLabel
+            // 
+            this.SubjectLabel.AutoSize = true;
+            this.SubjectLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.SubjectLabel.ForeColor = System.Drawing.Color.White;
+            this.SubjectLabel.Location = new System.Drawing.Point(12, 99);
+            this.SubjectLabel.Name = "SubjectLabel";
+            this.SubjectLabel.Size = new System.Drawing.Size(49, 18);
+            this.SubjectLabel.TabIndex = 1;
+            this.SubjectLabel.Text = "Tárgy:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(340, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(67, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(633, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(67, 126);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(633, 373);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // fileBtn
+            // 
+            this.fileBtn.Location = new System.Drawing.Point(67, 505);
+            this.fileBtn.Name = "fileBtn";
+            this.fileBtn.Size = new System.Drawing.Size(118, 23);
+            this.fileBtn.TabIndex = 6;
+            this.fileBtn.Text = "Fájl csatolása";
+            this.fileBtn.UseVisualStyleBackColor = true;
+            this.fileBtn.Click += new System.EventHandler(this.fileBtn_Click);
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(191, 505);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 7;
+            this.sendBtn.Text = "Küldés";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(114)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(63, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Email";
+            // 
+            // titleBox
+            // 
+            this.titleBox.FormattingEnabled = true;
+            this.titleBox.Location = new System.Drawing.Point(67, 73);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(633, 21);
+            this.titleBox.TabIndex = 9;
+            // 
+            // attachmentLabel
+            // 
+            this.attachmentLabel.AutoSize = true;
+            this.attachmentLabel.ForeColor = System.Drawing.Color.White;
+            this.attachmentLabel.Location = new System.Drawing.Point(64, 542);
+            this.attachmentLabel.Name = "attachmentLabel";
+            this.attachmentLabel.Size = new System.Drawing.Size(0, 13);
+            this.attachmentLabel.TabIndex = 10;
             // 
             // Email
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1118, 646);
+            this.Controls.Add(this.attachmentLabel);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.fileBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SubjectLabel);
+            this.Controls.Add(this.titleLabel);
             this.Name = "Email";
             this.Text = "Email";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label SubjectLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button fileBtn;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox titleBox;
+        private System.Windows.Forms.Label attachmentLabel;
     }
 }
--- MiniJobzAdminSite/Email.cs
+++ MiniJobzAdminSite/Email.cs
@@ -1,23 +1,140 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace MiniJobzAdminSite
 {
     public partial class Email : Form
     {
         private string username;
+        private string attachmentPath = "";
+
+        private string connectionString =
+            "Server=localhost;Database=minijobz_teszt;Uid=root;";
 
         public Email(string username)
         {
             InitializeComponent();
             this.username = username;
+            this.Load += Email_Load;
+        }
+
+        private void Email_Load(object sender, EventArgs e)
+        {
+            titleBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            LoadEmails();
+        }
+
+        private void LoadEmails()
+        {
+            try
+            {
+                titleBox.Items.Clear();
+
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query =
+                        "SELECT email FROM users WHERE email IS NOT NULL AND email != ''";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string email = reader["email"].ToString();
+                            titleBox.Items.Add(email);
+                        }
+                    }
+                }
+
+                if (titleBox.Items.Count > 0)
+                    titleBox.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Adatbázis hiba:\n" + ex.Message,
+                    "DB hiba",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+        }
+
+        private void fileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Csatolmány kiválasztása";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                attachmentPath = ofd.FileName;
+                attachmentLabel.Text = "Csatolt fájl: " + Path.GetFileName(attachmentPath);
+            }
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (titleBox.SelectedItem == null)
+                {
+                    MessageBox.Show("Nincs kiválasztott email!");
+                    return;
+                }
+
+                string selectedEmail = titleBox.SelectedItem.ToString();
+
+                MailMessage mail = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+
+                mail.From = new MailAddress("minii.jobzz@gmail.com");
+                mail.To.Add(selectedEmail);
+
+                mail.Subject = "Admin üzenet";
+                mail.Body = richTextBox1.Text;
+                mail.IsBodyHtml = false;
+
+                if (!string.IsNullOrEmpty(attachmentPath))
+                {
+                    mail.Attachments.Add(new Attachment(attachmentPath));
+                }
+
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential(
+                    "minii.jobzz@gmail.com",
+                    "pkdyspcljxgfcjgf"
+                );
+
+                smtp.Send(mail);
+
+                MessageBox.Show(
+                    "Email sikeresen elküldve!",
+                    "Siker",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+
+                attachmentPath = "";
+                attachmentLabel.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Email küldési hiba:\n" + ex.Message,
+                    "Hiba",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
         }
     }
 }
